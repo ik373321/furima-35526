2a43b25d741f46ab671dcbadd0e8ff7aeb08515b
ik373321 <82790121+ik373321@users.noreply.github.com>
2021-05-15 18:54:13 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,15 @@ class User < ApplicationRecord
  has_many :records
  
  with_options presence: true do
- validates :nickname
- validates :birthday
- PASSWORD_VALIDATES = PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
- validates :password, format: { with: PASSWORD_VALIDATES }
- NAME_KANA_VALIDATES = /\A[ァ-ヶー－]+\z/
- validates :last_name_kana, format: { with: NAME_KANA_VALIDATES}
- validates :first_name_kana, format: { with: NAME_KANA_VALIDATES}
- validates :password, confirmation: true
- NAME_VALIDATES = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
- validates :first_name, format: { with: NAME_VALIDATES}
- validates :last_name, format: { with: NAME_VALIDATES}
- end
+   validates :nickname, :birthday
+ with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
     it 'last_nameが空では登録できない' do
       @user.last_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last name is invalid")
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it 'last_nameが全角（漢字・ひらがな・カタカナ）以外では登録できない' do
@@ -32,7 +32,7 @@ RSpec.describe User, type: :model do
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name is invalid")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it 'first_nameが全角（漢字・ひらがな・カタカナ）以外では登録できない' do
