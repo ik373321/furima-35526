3fdb7166cff7e7c6a063494f73369d208c659e42
ik373321 <82790121+ik373321@users.noreply.github.com>
2021-05-16 10:35:44 +0900

    productモデル・マイグレーションファイル作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,17 @@ def index
 end
 
 def new
+  @prototype = Prototype.find(params[:id])
+
+  if @prototype.update(prototypes_params)
+    redirect_to prototype_path
+  else
+    render :edit
+  end
 end
 
 def create
+ 
 end
 
 
