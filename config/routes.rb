e8df99111abf6259bd82d67926bc0025904308cf
ik373321 <82790121+ik373321@users.noreply.github.com>
2021-05-18 20:01:08 +0900

    Revert "詳細から編集まで"
    This reverts commit c1f668f5fd5e8b332af4ed8f4d8e8e0f8b0be017.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"products#index"
-  resources :products, only: [:index,:new,:create,:edit,:update,:show,:destroy]
+  resources :products, only: [:index,:new,:create]
 
 end
